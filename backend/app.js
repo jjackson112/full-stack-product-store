2db628bb751196aa2bab44e846af5847f2a2ad5a
jjackson112 <jjackson11211@gmail.com>
2025-09-30 18:13:29 -0400

    auto restart - GET request

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,13 @@ import express from 'express';
 
 const app = express();
 
+// controller function
+// auto restart
+
+app.get("/", (req, res) => {
+    res.send("Server is ready");
+});
+
 app.listen(5000, () => {
     console.log("Server started at http://localhost:5000");
-})
+});
